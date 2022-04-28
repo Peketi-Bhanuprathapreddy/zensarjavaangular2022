2b99745d9eddac267fb829b361d87c33b1f52f37
Peketi-Bhanuprathapreddy <b.peketi@zensar.com>
2022-04-27 22:25:21 -0700

    push

---
+++ basics/datetime.java
@@ -0,0 +1,18 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+public class datetime {
+    public static void main(String[] args) {
+        System.out.println(LocalTime.now());
+       System.out.println(LocalDate.now());
+
+         System.out.println(LocalDateTime.now());
+         ZoneId.getAvailableZoneIds().forEach(System.out::println); // Collection
+
+        System.out.println(LocalTime.of(6, 5, 59));
+        System.out.println(LocalTime.now(ZoneId.of("Asia/Kolkata")));
+    }
+}
