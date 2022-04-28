7f62fe6e8767d7fbd80230ccb2e16dd60dc2d732
Peketi-Bhanuprathapreddy <b.peketi@zensar.com>
2022-04-27 20:40:07 -0700

    push

---
+++ basics/implements2.java
@@ -0,0 +1,29 @@
+public class implements2 implements implements1 {
+
+    @Override
+    public void eat() {
+        // TODO Auto-generated method stubsy
+        System.out.println("abba sairam");
+        
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    System.out.println("shoes comfort ga vunnay mastaru");
+    }
+
+    @Override
+    public void dance() {
+        // TODO Auto-generated method stub
+        System.out.println("neetyhone dance tonight");
+    }
+    
+    public static void main(String args[])
+    {
+        new implements2().eat();
+        new implements2().run();
+        new implements2().dance();
+    }
+}
