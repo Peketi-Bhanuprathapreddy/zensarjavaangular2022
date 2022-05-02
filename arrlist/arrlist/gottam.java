6b19027ca3657f94ba782dbcef2779b3852a44d7
Peketi-Bhanuprathapreddy <b.peketi@zensar.com>
2022-05-02 01:52:42 -0700

    done

---
+++ arrlist/arrlist/gottam.java
@@ -0,0 +1,20 @@
+package arrlist;
+import java.util.ArrayList;
+import java.util.List;
+    
+
+public static List<Integer> convertIntoList(){ 
+    public static void main(String[] args){
+    int[] arr = {10, 15, 45, 67, 98};
+
+    // ArrayList<Integer[]> list = Arrays.asList(arr);
+    List<Integer> list = new ArrayList<>();
+
+    for (int e : arr) {
+        list.add(e);
+        System.out.println(e);
+    }
+
+    return list;
+}
+}
