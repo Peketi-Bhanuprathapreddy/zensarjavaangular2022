3361f1ff4888ad74c7f02b07cc0939f075f44f16
Peketi-Bhanuprathapreddy <b.peketi@zensar.com>
2022-05-03 22:26:56 -0700

    perfect

---
+++ arrlist/arrlist/que.java
@@ -0,0 +1,25 @@
+package arrlist;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class que {
+  
+    public static void main(String[] args)
+    {
+      Queue<Integer> q= new ArrayDeque<>();
+      q.offer(5110);
+      q.offer(503);
+      q.offer(510);
+      q.offer(520);
+      q.poll();
+     System.out.println( "hi ra"+q.size());
+    
+      q.add(12);
+    // q.clear();
+    // q.remove();
+    // System.out.println( "head is"+q.element());
+     System.out.println( "head is"+q.peek());
+      q.forEach(System.out::println);
+    }
+    
+}
